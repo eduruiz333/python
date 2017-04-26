b0c58c25a0a952687038c29ba320a922898580e3
marcos-ruiz <marcos.ruiz.@easynvest.com.br>
2017-04-26 16:53:52 -0300

    fibonacci in python

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+fim = int(input('Digite um número para calcular a sequência de Fibonacci: '))
+def calculaFibonacci():
+    numero = fim
+    pri = 0
+    seg = 1
+    fib = seg + pri
+    print("Sequência de Fibonatti até: ", numero )
+
+    cont = 0
+
+    while cont <= fim:
+        print(fib)
+        pri = seg
+        seg = fib
+        fib = seg + pri
+        cont = cont + 1
+calculaFibonacci()
---
+++ while.py
@@ -0,0 +1,7 @@
+quantasVezes = int(input('Digite a quantidade de vezes que quer calcular a potencia de 2: '))
+
+i = 0
+
+while i <= quantasVezes:
+    print(2 ** i)
+    i = i + 1
