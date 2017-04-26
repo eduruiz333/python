d6053bd9f2152f9d45e6e89153b10fa8cb9448cc
marcos-ruiz <marcos.ruiz.@easynvest.com.br>
2017-04-26 15:47:51 -0300

    novos exercicios

---
+++ booleanos-e-precedencias.py
@@ -0,0 +1,15 @@
+x = 10
+y = 20
+
+# no python, podemos usar as palavras reservadas "and", "or" e "not" ou então os sinais "&", "|" e "!="
+
+taSol = True
+ehFeriado = False
+
+vouNoRole = taSol | ehFeriado
+print(vouNoRole)
+
+temGrana = True
+ganhouPromo = False
+vouAoShow = temGrana or ganhouPromo
+print(vouAoShow)
---
+++ condicionais.py
@@ -0,0 +1,8 @@
+temperatura = float(input('Qual é a temperatura da água? '))
+
+if temperatura > 100:
+    print('A água está fervendo')
+    print('Já pode fazer o café')
+else:
+    print('A água ainda não está ferfendo')
+    print('Aguarde a água ferver para poder fazer o café')
