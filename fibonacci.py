39ee66d28996439756c86a3f1684265529ee6112
eduruiz333 <eduruiz333@gmail.com>
2017-04-26 20:16:10 -0300

    correção de padrão

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,12 @@
 fim = int(input('Digite um número para calcular a sequência de Fibonacci: '))
+
+
 def calculaFibonacci():
     numero = fim
     pri = 0
     seg = 1
     fib = seg + pri
-    print("Sequência de Fibonatti até: ", numero )
+    print("Sequência de Fibonatti até: ", numero)
 
     cont = 0
 
