7f7a8b32e0a90d11524190d4d9f740544d75793c
marcos-ruiz <marcos.ruiz@easynvest.com.br>
2017-05-03 10:53:28 -0300

    Refatoração do código

--- bhaskara.py
+++ bhaskara.py
@@ -1,19 +1,27 @@
 import math
 
-a = float(input('Digite o valor de A: '))
-b = float(input('Digite o valor de B: '))
-c = float(input('Digite o valor de C: '))
 
-delta = b ** 2 - 4 * a * c
+def delta(a, b, c):
+    return b ** 2 - 4 * a * c
 
-if delta == 0:
-    raiz1 = (-b + math.sqrt(delta)) / (2 * a)
-    print('A única raiz é: ', raiz1)
-else:
-    if delta < 0:
-        print('Essa equação não possui raízes reais')
+
+def main():
+    a = float(input('Digite o valor de A: '))
+    b = float(input('Digite o valor de B: '))
+    c = float(input('Digite o valor de C: '))
+    imprime_raizes(a, b, c)
+
+
+def imprime_raizes(a, b, c):
+    d = delta(a, b, c)
+    if d == 0:
+        raiz1 = (-b + math.sqrt(d)) / (2 * a)
+        print('A única raiz é: ', raiz1)
     else:
-        raiz1 = (-b + math.sqrt(delta)) / (2 * a)
-        raiz2 = (-b - math.sqrt(delta)) / (2 * a)
-        print('A primeira raiz é: ', raiz1)
-        print('A segunda raiz é: ', raiz2)
+        if d < 0:
+            print('Essa equação não possui raízes reais')
+        else:
+            raiz1 = (-b + math.sqrt(d)) / (2 * a)
+            raiz2 = (-b - math.sqrt(d)) / (2 * a)
+            print('A primeira raiz é: ', raiz1)
+            print('A segunda raiz é: ', raiz2)
