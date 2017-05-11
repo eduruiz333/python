a89c7984a874fd5f0ba203c4f33a7e04cb144f46
marcos-ruiz <marcos.ruiz@easynvest.com.br>
2017-05-11 11:17:35 -0300

    concituação do exercício, mas ainda não finalizado

--- matrizes.py
+++ matrizes.py
@@ -4,17 +4,24 @@ A = [
     [4, 5, 6],
     [7, 8, 9]
 ]
-
 print(A[2][0])
 """
 
 
-def cria_matriz(num_linhas, num_colunas, valor):  # (int, int, int)
+def cria_matriz(num_linhas, num_colunas, valor):  # (int, int, value)
+
     matriz = []
     for x in range(num_linhas):
         linha = []
         for y in range(num_colunas):
+            valor = int(input('Digite o elemento [' + str(x) + '][' + str(y) + ']'))
             linha.append(valor)
         matriz.append(linha)
-    return print(matriz)
-cria_matriz(5, 8, ' :-) ')
+    return matriz
+
+
+def le_matriz():
+    lin = int(input('Digite a quantidade de linhas: '))
+    col = int(input('Digite a quantidade de colunas: '))
+    return cria_matriz(lin, col)
+le_matriz()
